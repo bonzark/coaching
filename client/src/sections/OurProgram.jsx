98d7e1a6b181de7cd232a5b8ad128916cdb22312
bonzark <hello@bonzark.com>
2023-09-12 16:13:02 +0530

    changed the section content

--- client/src/sections/OurProgram.jsx
+++ client/src/sections/OurProgram.jsx
@@ -1,153 +1,144 @@
+import { Box, Grid, Typography } from "@mui/material";
 import React from "react";
-import Carousel from "react-multi-carousel";
-import ProgramCard from "../components/ProgramCard";
-import { Box, Typography } from "@mui/material";
-// import { ArrowBackIcon } from "@mui/icons-material/ArrowBack";
-// import { ArrowForwardIcon } from "@mui/icons-material/ArrowForward";
-
-const items = [
-  {
-    title: "Book Your £19.99 Energy And Life Path Reading",
-    btnText: "BOOK YOUR READING Special Offer Just at £19.99",
-    imgSrc: "./Coach1.jpg",
-  },
-  {
-    title: "Book Your £19.99 Energy And Life Path Reading",
-    btnText: "BOOK YOUR READING Special Offer Just at £19.99",
-    imgSrc: "./successStories.jpg",
-  },
-  {
-    title: "Book Your £19.99 Energy And Life Path Reading",
-    btnText: "BOOK YOUR READING Special Offer Just at £19.99",
-    imgSrc: "./Coach1.jpg",
-  },
-  {
-    title: "Book Your £19.99 Energy And Life Path Reading",
-    btnText: "BOOK YOUR READING Special Offer Just at £19.99",
-    imgSrc: "./Coach2.jpg",
-  },
-  {
-    title: "Book Your £19.99 Energy And Life Path Reading",
-    btnText: "BOOK YOUR READING Special Offer Just at £19.99",
-    imgSrc: "./successStories.jpg",
-  },
-];
+import { PrimaryBtn } from "../components/PrimaryBtn";
+import { Link } from "react-router-dom";
+import { ourProgramsData } from "../utils/constant";
 
 const OurProgram = () => {
   return (
     <Box
       sx={{
-        background: "linear-gradient(#D7CCE4, #DCD9F0)",
-        py: "100px",
-        px: { xs: "25px", sm: "30px" },
+        minHeight: "100vh",
+        display: "grid",
+        paddingX: { xs: "1rem", md: "5rem", lg: "10rem" },
       }}
     >
-      <Box
-        sx={{ textAlign: "center", display: "flex", flexDirection: "column" }}
-      >
-        <Typography
-          className="borderTitle"
-          variant="span"
-          sx={{
-            position: "relative",
-            fontSize: { xs: "32px", sm: "48px" },
-            color: "#671d63",
-            lineHeight: "48px",
-            fontFamily: "'Abril FatFace', sans-serif",
-            display: "flex",
-            marginBottom: { xs: "30px", md: "50px" },
-            justifyContent: "center",
-            "::before": {
-              position: "absolute",
-              bottom: { xs: "-18px", md: "-27px" },
-              content: `""`,
-              height: "3px",
-              width: "110px",
-              backgroundColor: "#671d63",
-            },
-          }}
-        >
-          Our Programs
-        </Typography>
-        <Typography
-          className="borderTitle"
-          variant="span"
-          sx={{
-            fontSize: { xs: "16px", sm: "22px" },
-            lineHeight: "30px",
-            paddingTop: "20px",
-            paddingBottom: { sm: "15px", md: "20px" },
-          }}
-        >
-          Are You Ready To Take Control Of Your Life and Manifest The Life You
-          Truly Desire?
-        </Typography>
-      </Box>
-      <Box sx={{ mx: { sx: "25px", md: "45px", lg: "130px" } }}>
-        <Carousel
-          sx={{ margin: "30px" }}
-          additionalTransfrom={0}
-          arrows
-          autoPlaySpeed={3000}
-          centerMode={false}
-          className=""
-          containerClass="container"
-          // customLeftArrow={<ArrowBackIcon />}
-          // customRightArrow={<ArrowForwardIcon />}
-          dotListClass=""
-          focusOnSelect={false}
-          infinite
-          itemClass=""
-          keyBoardControl
-          minimumTouchDrag={80}
-          pauseOnHover
-          renderArrowsWhenDisabled={false}
-          renderButtonGroupOutside={false}
-          renderDotsOutside={false}
-          rewind={false}
-          rewindWithAnimation={false}
-          rtl={false}
-          shouldResetAutoplay
-          showDots={false}
-          sliderClass="OurProgramCardItem"
-          slidesToSlide={1}
-          swipeable
-          responsive={{
-            desktop: {
-              breakpoint: {
-                max: 3000,
-                min: 976,
-              },
-              items: 2,
-              partialVisibilityGutter: 20,
-            },
-            tablet: {
-              breakpoint: {
-                max: 976,
-                min: 464,
-              },
-              items: 2,
-              partialVisibilityGutter: 30,
-            },
-            mobile: {
-              breakpoint: {
-                max: 464,
-                min: 0,
-              },
-              items: 1,
-              partialVisibilityGutter: 10,
-            },
-          }}
-        >
-          {items.map((item, index) => (
-            <ProgramCard
-              key={index}
-              btnText={item.btnText}
-              title={item.title}
-              imgSrc={item.imgSrc}
-            />
-          ))}
-        </Carousel>
+      <Box>
+        {ourProgramsData.map((item) => (
+          <Grid
+            key={item.title}
+            container
+            sx={{
+              width: "100%",
+              height: "100%",
+              justifyContent: "space-around",
+              display: "flex",
+              flexDirection: "row-reverse",
+            }}
+          >
+            <Grid
+              item
+              xs={12}
+              md={6}
+              lg={6}
+              sx={{
+                display: "flex",
+                justifyContent: "flex-start",
+                alignItems: "center",
+              }}
+            >
+              <Box
+                sx={{
+                  height: {
+                    xs: "275px",
+                    sm: "375px",
+                    md: "400px",
+                    xl: "600px",
+                  },
+                  width: { xs: "275px", sm: "375px", md: "400px", xl: "600px" },
+                  margin: { xs: "0 auto", md: "0 0 0 auto" },
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                  objectFit: "cover",
+                  position: "relative",
+                }}
+              >
+                <img
+                  style={{
+                    width: "100%",
+                    height: "100%",
+                    objectFit: "cover",
+                    borderRadius: "50%",
+                    objectPosition: "top center",
+                  }}
+                  src={item.imgSrc}
+                  alt="brandi"
+                />
+              </Box>
+            </Grid>
+            <Grid
+              item
+              xs={12}
+              md={6}
+              lg={6}
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "flex-start",
+                flexDirection: "column",
+                gap: "20px",
+                fontSize: "20px",
+                lineHeight: "24px",
+                padding: { lg: "0 0 0 30px" },
+              }}
+            >
+              <Typography
+                component={"h1"}
+                sx={{
+                  position: "relative",
+                  color: "#671d63",
+                  fontSize: { xs: "30px", md: "42px" },
+                  fontFamily: "'Abril Fatface', cursive",
+                  "::before": {
+                    position: "absolute",
+                    bottom: "0",
+                    content: `""`,
+                    height: "5px",
+                    width: "100px",
+                    backgroundColor: "goldenrod",
+                  },
+                }}
+              >
+                {item.title}
+              </Typography>
+              <Typography
+                sx={{
+                  fontSize: {
+                    sx: "16px",
+                    md: "20px",
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "space-between",
+                  },
+                }}
+              >
+                {item.contentProg}
+              </Typography>
+              <Typography
+                to={"know-more"}
+                component={Link}
+                sx={{
+                  color: "#671d63",
+                  textDecoration: "none",
+                  fontSize: "16px",
+                }}
+              >
+                {item.linkText}
+              </Typography>
+              <PrimaryBtn>
+                <Box sx={{ padding: "0 40px" }}>
+                  <Typography sx={{ fontSize: "16px", fontWeight: 900 }}>
+                    {item.btnText1}
+                  </Typography>
+                  <Typography sx={{ fontSize: "16px", fontWeight: 900 }}>
+                    {item.btnText2}
+                  </Typography>
+                </Box>
+              </PrimaryBtn>
+            </Grid>
+          </Grid>
+        ))}
       </Box>
     </Box>
   );
