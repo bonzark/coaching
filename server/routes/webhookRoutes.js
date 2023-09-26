5ce84df140f8a02a84c4dc91e2813a0cf928654a
bonzark <hello@bonzark.com>
2023-09-26 14:20:18 +0530

    small fix

--- server/routes/webhookRoutes.js
+++ server/routes/webhookRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const paymentController = require("../controllers/paymentController");
+const sessionController = require("../controllers/sessionController");
 
 // Route to payment
 router.post(
