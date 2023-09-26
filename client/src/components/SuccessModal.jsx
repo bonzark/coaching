9e492816505df8ca4e3dc2b4c0b881eebca2a771
bonzark <hello@bonzark.com>
2023-09-26 17:03:31 +0530

    updated success-card

--- client/src/components/SuccessModal.jsx
+++ client/src/components/SuccessModal.jsx
@@ -1,11 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
-  Button,
   Dialog,
-  DialogActions,
+  Box,
+  DialogTitle,
   DialogContent,
   DialogContentText,
-  DialogTitle,
+  DialogActions,
+  Button,
   LinearProgress,
   Typography,
 } from "@mui/material";
@@ -13,67 +14,52 @@ import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 
 const SuccessModal = ({ open, handleClose, successMessage }) => {
   const [progress, setProgress] = useState(0);
-
-  useEffect(() => {
-    let timer;
-
-    if (open) {
-      timer = setInterval(() => {
-        setProgress((prevProgress) => {
-          if (prevProgress >= 100) {
-            clearInterval(timer);
-            handleClose();
-            // navigate("/");
-          } else {
-            return prevProgress + 10;
-          }
-        });
-      }, 300); // Adjust the interval as needed
-    }
-
-    return () => {
-      clearInterval(timer);
-    };
-  }, [open, handleClose]);
-
-  useEffect(() => {
-    if (!open) {
-      setProgress(0);
-    }
-  }, [open]);
-
+  if (open) {
+    setTimeout(() => {
+      handleClose();
+    }, 3000);
+  }
   return (
     <Dialog open={open} onClose={handleClose}>
-      <DialogTitle>
-        <CheckCircleIcon
-          sx={{ fill: "limegreen", width: "40px", height: "40px" }}
-        />
-        <Typography>Payment Done SuccessFully </Typography>
-      </DialogTitle>
-      <DialogContent sx={{ padding: "10px 25px" }}>
-        {successMessage && (
-          <DialogContentText>{successMessage}</DialogContentText>
-        )}
+      <Box
+        sx={{
+          width: "450px",
+          borderRadius: "15px",
+        }}
+      >
+        <DialogTitle>
+          <CheckCircleIcon
+            sx={{ fill: "limegreen", width: "40px", height: "40px" }}
+          />
+          <Typography sx={{ fontSize: { xs: "1.2rem", md: "2rem" } }}>
+            Payment Done Successfully
+          </Typography>
+        </DialogTitle>
+        <DialogContent sx={{ padding: "10px 25px" }}>
+          <DialogContentText sx={{ fontSize: { xs: "0.9rem", md: "1rem" } }}>
+            {successMessage}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions sx={{ padding: "10px 25px" }}>
+          <Button
+            sx={{
+              color: "#673d61",
+              mt: "1rem",
+              border: "1px solid #673d61",
+              padding: "5px 25px",
+            }}
+            onClick={handleClose}
+            color="primary"
+          >
+            Close
+          </Button>
+        </DialogActions>
         <LinearProgress
-          value={progress}
           variant="determinate"
-          sx={{ color: "#673d61", mt: "1rem" }}
+          value={progress}
+          sx={{ color: "#673d61", mt: "1rem", mb: "1px" }}
         />
-      </DialogContent>
-      <DialogActions sx={{ padding: "10px 25px" }}>
-        <Button
-          sx={{
-            color: "#673d61",
-            mt: "1rem",
-            border: "1px solid #673d61",
-            padding: "5px 25px",
-          }}
-          onClick={handleClose}
-          color="primary"
-        >
-          Close
-        </Button>
-      </DialogActions>
+      </Box>
     </Dialog>
   );
 };
--- client/src/pages/home/home.jsx
+++ client/src/pages/home/home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import HeroBanner from "../../sections/heroBanner";
 import SuccessCarousel from "../../sections/SuccessCarousel";
 import AboutSection from "../../sections/AboutSection";
@@ -8,8 +8,14 @@ import OurCoaches from "../../sections/OurCoaches";
 import OurProgram from "../../sections/OurProgram";
 import OfferSection from "../../sections/OffersSection";
 import { heroListItems } from "../../utils/constant";
+import SuccessModal from "../../components/SuccessModal";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const { search } = useLocation();
+  const [isOpen, setIsOpen] = useState(search === "?success=true");
+  const navigate = useNavigate();
+
   return (
     <>
       <HeroBanner
@@ -17,6 +23,14 @@ const Home = () => {
         title={"Manifesting and living life you desire !"}
         imageUrl="./heroBg2.jpg"
         listItems={heroListItems}
+      />
+      <SuccessModal
+        successMessage={"Congratulations, Your session has been purchased."}
+        open={isOpen}
+        handleClose={() => {
+          navigate("/");
+          setIsOpen(false);
+        }}
       />
       <AboutSection />
       <OurCoaches />
