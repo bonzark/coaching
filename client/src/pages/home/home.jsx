bf50244fbc4c7af6aa2bee7e07a1d438c9fe1597
bonzark <hello@bonzark.com>
2023-09-26 17:04:33 +0530

    Cancel modal FE partial

--- client/src/pages/home/home.jsx
+++ client/src/pages/home/home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import HeroBanner from "../../sections/heroBanner";
 import SuccessCarousel from "../../sections/SuccessCarousel";
 import AboutSection from "../../sections/AboutSection";
@@ -8,10 +8,23 @@ import OurCoaches from "../../sections/OurCoaches";
 import OurProgram from "../../sections/OurProgram";
 import OfferSection from "../../sections/OffersSection";
 import { heroListItems } from "../../utils/constant";
+import CancelModal from "../../sections/CancelModal";
+import { useLocation, useNavigate, useSearchParams } from "react-router-dom";
 
 const Home = () => {
+  const { search } = useLocation();
+  const navigate = useNavigate();
+  const [open, setOpen] = useState(search === "?cancel=true");
   return (
     <>
+      <CancelModal
+        cancelMessage={"payment cancelled!!!"}
+        open={open}
+        onClose={() => {
+          navigate("/");
+          setOpen(false);
+        }}
+      />
       <HeroBanner
         header={"Discover what is blocking you from"}
         title={"Manifesting and living life you desire !"}
