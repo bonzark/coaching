8ba7dabe69062df562932d47c8ae2b311dfa5da5
Bonzark <hello@bonzark.com>
2023-09-28 13:59:44 +0530

    changes in payment cancel text

--- server/controllers/paymentController.js
+++ server/controllers/paymentController.js
@@ -1,8 +1,8 @@
-const stripe = require("stripe")(process.env.STRIPE_SECRET);
-const express = require("express");
-const PaymentDetail = require("../models/paymentDetails");
-const BookedSession = require("../models/bookedSession");
-const User = require("../models/user");
+const stripe = require('stripe')(process.env.STRIPE_SECRET);
+const express = require('express');
+const PaymentDetail = require('../models/paymentDetails');
+const BookedSession = require('../models/bookedSession');
+const User = require('../models/user');
 const app = express();
 
 const CLIENT_URL = process.env.HOST_URL;
@@ -13,19 +13,19 @@ exports.paymentSession = async (req, res) => {
     const sessionId = req.body.sessionId;
 
     const session = await stripe.checkout.sessions.create({
-      payment_method_types: ["card"],
-      mode: "payment",
+      payment_method_types: ['card'],
+      mode: 'payment',
       line_items: [
         {
           //Price Id can be retrived from products page, a single product will have a unique priceId
-          price: req.body.price_id ?? "price_1NtwGWSFygNTJvTO4irLPEiE",
+          price: req.body.price_id ?? 'price_1NtwGWSFygNTJvTO4irLPEiE',
           quantity: 1,
         },
       ],
       // On success, redirect to:
       success_url: `${CLIENT_URL}?success=true`,
       // On failure, redirect to:
-      cancel_url: `${CLIENT_URL}?canceled=true`,
+      cancel_url: `${CLIENT_URL}?cancelled=true`,
       metadata: {
         userId: userId,
         sessionId: sessionId,
@@ -35,7 +35,7 @@ exports.paymentSession = async (req, res) => {
     //Redirect user to this url. It is a stripe url, payment page.
     res.json({ url: session.url });
   } catch (e) {
-    console.log("error", e);
+    console.log('error', e);
     res.status(500).json({ error: e.message });
   }
 };
@@ -45,7 +45,7 @@ exports.paymentCompleted = async (req, res) => {
 
   // Verify the webhook event (optional but recommended)
   try {
-    const signature = req.headers["stripe-signature"];
+    const signature = req.headers['stripe-signature'];
     const verifiedEvent = stripe.webhooks.constructEvent(
       event,
       signature,
@@ -54,12 +54,10 @@ exports.paymentCompleted = async (req, res) => {
 
     // Store the event in MongoDB
 
-    if (verifiedEvent.type === "payment_intent.succeeded") {
+    if (verifiedEvent.type === 'payment_intent.succeeded') {
       const paymentIntent = verifiedEvent.data.object;
 
-      const checkoutSession = await getCheckoutSessionByPaymentIntentId(
-        paymentIntent.id
-      );
+      const checkoutSession = await getCheckoutSessionByPaymentIntentId(paymentIntent.id);
 
       const userId = checkoutSession.metadata.userId;
       const sessionId = checkoutSession.metadata.sessionId;
@@ -76,7 +74,7 @@ exports.paymentCompleted = async (req, res) => {
         session: sessionId,
         user: userId,
         purchaseDate: paymentIntent.created,
-        status: "purchased",
+        status: 'purchased',
       });
       await paymentDetail.save();
       await purchasedSession.save();
@@ -85,7 +83,7 @@ exports.paymentCompleted = async (req, res) => {
     }
     res.status(200).redirect(process.env.HOST_URL);
   } catch (error) {
-    console.error("Error handling webhook event:", error);
+    console.error('Error handling webhook event:', error);
     res.sendStatus(400); // Respond with an error status code
   }
 };
@@ -102,7 +100,7 @@ async function getCheckoutSessionByPaymentIntentId(paymentIntentId) {
 
     return session;
   } catch (error) {
-    console.error("Error retrieving Checkout Session:", error);
+    console.error('Error retrieving Checkout Session:', error);
     throw error;
   }
 }
