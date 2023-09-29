579b125b95c449f110e09cd83cf46bd35bcb6405
bonzark <hello@bonzark.com>
2023-09-29 16:58:38 +0530

    Previous Purchases Page FE

--- client/src/pages/dashboard/dashboard.jsx
+++ client/src/pages/dashboard/dashboard.jsx
@@ -1,10 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import { Box, Grid, Typography } from '@mui/material';
-import CircularProgress from '@mui/material/CircularProgress';
-import { sessionData, subscriptionData } from '../../utils/constant';
-import SessionCard from '../../components/SessionCard';
-import SubscriptionCard from '../../components/SubscriptionCard';
-import { getBookedSessions, getSessions } from '../../services/session.service';
+import React, { useEffect, useState } from "react";
+import { Box, Grid, Typography } from "@mui/material";
+import SessionCard from "../../components/SessionCard";
+import { getBookedSessions, getSessions } from "../../services/session.service";
 
 const Dashboard = () => {
   const [upcomingSessions, setUpcomingSessions] = useState([]);
@@ -20,9 +17,17 @@ const Dashboard = () => {
   }, []);
   const renderUpcomingSessions = (upcomingSessions) => {
     return upcomingSessions?.map((session) => {
+      console.log("SESSION::", session);
       return (
         <>
-          <Grid key={session?._id} sx={{ height: '100% !important' }} item xs={12} sm={6} lg={4}>
+          <Grid
+            key={session?._id}
+            sx={{ height: "100% !important" }}
+            item
+            xs={12}
+            sm={6}
+            lg={4}
+          >
             <SessionCard
               price={session?.session?.price}
               date={session?.date}
@@ -37,42 +42,42 @@ const Dashboard = () => {
       );
     });
   };
-  const user = JSON.parse(localStorage.getItem('user'))?.name;
+  const user = JSON.parse(localStorage.getItem("user"))?.name;
   return (
     <Box
       sx={{
-        marginTop: { xs: '40px', md: '92px' },
-        padding: { xs: '1rem', sm: '2rem', md: '3rem', lg: '4rem' },
+        marginTop: { xs: "40px", md: "92px" },
+        padding: { xs: "1rem", sm: "2rem", md: "3rem", lg: "4rem" },
       }}
     >
       <Typography
         sx={{
-          fontSize: { xs: '33px', md: '42px' },
-          marginTop: '15px',
+          fontSize: { xs: "33px", md: "42px" },
+          marginTop: "15px",
           fontFamily: "'montserrat', cursive",
         }}
       >
         Welcome,
         <Typography
           sx={{
-            color: '#673d61',
-            pl: '10px',
-            fontSize: { xs: '33px', md: '42px' },
-            textTransform: 'capitalize',
+            color: "#673d61",
+            pl: "10px",
+            fontSize: { xs: "33px", md: "42px" },
+            textTransform: "capitalize",
           }}
-          component={'span'}
+          component={"span"}
         >
           {user}
         </Typography>
       </Typography>
-      <Box sx={{ pt: '15px' }}>
+      <Box sx={{ pt: "15px" }}>
         <Typography
           sx={{
-            fontSize: { xs: '20px', sm: '25px', md: '30px' },
-            marginTop: '15px',
-            color: '#673d61',
+            fontSize: { xs: "20px", sm: "25px", md: "30px" },
+            marginTop: "15px",
+            color: "#673d61",
             fontFamily: "'montserrat', cursive",
-            textAlign: 'center',
+            textAlign: "center",
           }}
         >
           Upcoming Sessions
@@ -82,8 +87,8 @@ const Dashboard = () => {
             spacing={3}
             container
             sx={{
-              marginTop: '0 !important',
-              paddingBottom: '25px',
+              marginTop: "0 !important",
+              paddingBottom: "25px",
             }}
           >
             {/* {sessionData?.map((session) => (
--- client/src/pages/previousPurchases/previousPurchases.jsx
+++ client/src/pages/previousPurchases/previousPurchases.jsx
@@ -1,64 +1,103 @@
+import { useEffect, useState } from "react";
 import { Box } from "@mui/material";
+import { getUserDetails, setUserDetails } from "../../utils/auth";
+import { PopupModal } from "react-calendly";
+import { getuserById } from "../../services/user.service";
+import { getAllSessions } from "../../services/session.service";
 import SessionCard from "../../components/SessionCard";
 import PageBanner from "../../sections/PageBanner";
 
 const PreviousPurchases = () => {
-  const sessions = [
-    {
-      title: "This is title 1",
-      detail:
-        "Lorem ipsum is a dummy text for a dummy content that can be used has a paragraph placeholder.",
-      sessionLink: "https://google.com",
-    },
-    {
-      title: "This is title 2",
-      detail:
-        "Lorem ipsum is a dummy text for a dummy content that can be used has a paragraph placeholder.",
-      sessionLink: "https://google.com",
-    },
-    {
-      title: "This is title 3",
-      detail:
-        "Lorem ipsum is a dummy text for a dummy content that can be used has a paragraph placeholder.",
-      sessionLink: "https://google.com",
-    },
-    {
-      title: "This is title 4",
-      detail:
-        "Lorem ipsum is a dummy text for a dummy content that can be used has a paragraph placeholder.",
-      sessionLink: "https://google.com",
-    },
-    {
-      title: "This is title 5",
-      detail:
-        "Lorem ipsum is a dummy text for a dummy content that can be used has a paragraph placeholder.",
-      sessionLink: "https://google.com",
-    },
-    {
-      title: "This is title 6",
-      detail:
-        "Lorem ipsum is a dummy text for a dummy content that can be used has a paragraph placeholder.",
-      sessionLink: "https://google.com",
-    },
-    {
-      title: "This is title 7",
-      detail:
-        "Lorem ipsum is a dummy text for a dummy content that can be used has a paragraph placeholder.",
-      sessionLink: "https://google.com",
-    },
-    {
-      title: "This is title 8",
-      detail:
-        "Lorem ipsum is a dummy text for a dummy content that can be used has a paragraph placeholder.",
-      sessionLink: "https://google.com",
-    },
-    {
-      title: "This is title 9",
-      detail:
-        "Lorem ipsum is a dummy text for a dummy content that can be used has a paragraph placeholder.",
-      sessionLink: "https://google.com",
-    },
+  const [sessionLinks, setSessionLinks] = useState({});
+  const [popup, setPopup] = useState(false);
+  const [popupLink, setPopupLink] = useState("");
+  const ids = {};
+  const userDetail = getUserDetails();
+  const ownedSessions = [
+    ...userDetail.bookedSession,
+    ...userDetail.purchasedSession,
   ];
+  ownedSessions
+    .filter((session) => session.status === "booked" && session)
+    .forEach((session) => {
+      ids[[session._id]] = false;
+    });
+
+  useEffect(() => {
+    setSessionLinks(ids);
+  }, []);
+  useEffect(() => {
+    if (window.location.pathname === "/our-coaches") {
+      setIsCoachPage(true);
+      getSessionsByCoachID(coachId)
+        .then((res) => {
+          purchaseSession(userDetails, res?.data?.sessions);
+          bookedSession(userDetails, res?.data?.sessions);
+        })
+        .catch((err) => console.log(err));
+    } else {
+      getAllSessions()
+        .then((res) => {
+          purchaseSession(userDetails, res?.data?.sessions);
+          bookedSession(userDetails, res?.data?.sessions);
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [open]);
+
+  const bookedSession = (userDetails, data) => {
+    const bookedSession = data;
+    const sessionsWithLink = {};
+    if (userDetails && bookedSession) {
+      const data = userDetails?.bookedSession;
+      for (let i = 0; i < bookedSession.length; i++) {
+        for (let j = 0; j < data.length; j++) {
+          if (data[j].session === bookedSession[i]._id) {
+            bookedSession[i].isBooked = true;
+            bookedSession[i].sessionLink = data[j].link;
+          }
+        }
+      }
+      bookedSession.forEach((session) => {
+        if (session.isBooked) sessionsWithLink[[session._id]] = false;
+      });
+    }
+  };
+
+  const purchaseSession = (userDetails, data) => {
+    const purchaseSession = data;
+    if (userDetails && purchaseSession) {
+      const data = userDetails?.purchasedSession;
+      for (let i = 0; i < purchaseSession.length; i++) {
+        for (let j = 0; j < data.length; j++) {
+          if (data[j].session === purchaseSession[i]._id) {
+            purchaseSession[i].isPurchased = true;
+          }
+        }
+      }
+    }
+  };
+
+  const bookHandler = (data) => {
+    console.log("data :", data);
+    setPopupLink(data?.session?.calendlyLink);
+    setPopup(true);
+  };
+
+  const popupCloseHandler = async () => {
+    setPopup(false);
+    if (userDetail) {
+      const { data } = await getuserById(userDetail._id);
+      setUserDetails(data);
+      getAllSessions()
+        .then((res) => {
+          purchaseSession(userDetails, res?.data?.sessions);
+          bookedSession(userDetails, res?.data?.sessions);
+        })
+        .catch((err) => console.log(err));
+    }
+  };
+
   return (
     <Box
       sx={{
@@ -77,21 +116,46 @@ const PreviousPurchases = () => {
           justifyContent: "center",
         }}
       >
-        {sessions.map((session, index) => (
-          <Box sx={{ maxWidth: "400px" }}>
-            <SessionCard
-              key={index}
-              title={session.title}
-              detail={session.detail}
-              sessionLink={session.sessionLink}
-              btnText="Click me"
-              onClick={() => {
-                console.log("Clicked!!");
-              }}
-            />
-          </Box>
-        ))}
+        {ownedSessions.map(
+          (session, index) =>
+            (session.status === "booked" || session.status === "purchased") && (
+              <Box key={session?._id} sx={{ maxWidth: "400px" }}>
+                <SessionCard
+                  key={session._id + index}
+                  title={session.session.title}
+                  detail={session.session.details}
+                  sessionLink={
+                    sessionLinks[session._id] &&
+                    session.status === "booked" &&
+                    session.session.calendlyLink
+                  }
+                  btnText={
+                    !sessionLinks[session._id]
+                      ? session?.status === "booked"
+                        ? "Get Link"
+                        : "Book Now"
+                      : ""
+                  }
+                  onClick={() => {
+                    if (session.status === "booked")
+                      setSessionLinks((prev) => ({
+                        ...prev,
+                        [session._id]: true,
+                      }));
+                    else bookHandler(session);
+                  }}
+                />
+              </Box>
+            )
+        )}
       </Box>
+      <PopupModal
+        url={popupLink}
+        prefill={{ email: userDetail?.email, name: userDetail?.name }}
+        onModalClose={popupCloseHandler}
+        open={popup}
+        rootElement={document.getElementById("root")}
+      />
     </Box>
   );
 };
