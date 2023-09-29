e0968db8f4952744c83ba4f04c4be8c0b7cb14c1
Bonzark <hello@bonzark.com>
2023-09-29 16:11:03 +0530

    ui changes fixed

--- client/src/pages/ourCoaches/ourCoachesDetail.jsx
+++ client/src/pages/ourCoaches/ourCoachesDetail.jsx
@@ -1,47 +1,51 @@
-import { Box, Typography } from "@mui/material";
-import React, { useEffect, useState } from "react";
-import { useLocation, useParams } from "react-router-dom";
-import SingleCoachDetail from "../../sections/SingleCoachDetail";
-import { getCoaches } from "../../services/session.service";
+import { Box, Typography } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import SingleCoachDetail from '../../sections/SingleCoachDetail';
+import { getCoaches } from '../../services/session.service';
 
 const OurCoachesDetail = () => {
-  const location = useLocation();
-  const { name } = location.state;
   const { id } = useParams();
 
-  console.log("id", id);
-
   const [coachesData, setCoachesData] = useState([]);
-  useEffect(() => {
-    const coachesDataList = getCoaches();
-    setCoachesData(coachesDataList);
-  }, [id]);
 
-  console.log("Data :", coachesData);
-  // const data = coachesData?.find((coach) => coach.id === id);
+  useEffect(() => {
+    getCoaches()
+      .then((res) => {
+        if (res?.status === 200) {
+          const data = res?.data?.coaches?.filter((coach) => coach._id === id);
+          if (data && data.length > 0) {
+            setCoachesData(data[0]);
+          }
+        }
+      })
+      .catch((err) => {
+        console.log('err', err);
+      });
+  }, [id]);
 
   return (
     <>
       <Box
         sx={{
-          margin: "5vh 0",
+          margin: '5vh 0',
           padding: {
-            xs: "4rem 1rem 0",
-            sm: "4rem 2rem 0",
-            md: "4rem 3rem 0",
+            xs: '4rem 1rem 0',
+            sm: '4rem 2rem 0',
+            md: '4rem 3rem 0',
           },
-          background: "linear-gradient(#DCD9F0,#ffffff)",
+          background: 'linear-gradient(#DCD9F0,#ffffff)',
         }}
       >
         <Typography
           variant="h1"
           sx={{
-            textAlign: "center",
+            textAlign: 'center',
             fontSize: {
-              xs: "1.5rem",
-              sm: "2rem",
-              md: "2.5rem",
-              lg: "3rem",
+              xs: '1.5rem',
+              sm: '2rem',
+              md: '2.5rem',
+              lg: '3rem',
             },
           }}
         >
@@ -49,34 +53,34 @@ const OurCoachesDetail = () => {
             variant="span"
             sx={{
               fontFamily: "'montserrat', sans-seri",
-              color: "#671D63",
+              color: '#671D63',
             }}
           >
             Meet Our Coach &nbsp;
             <Typography
               variant="span"
               sx={{
-                position: "relative",
-                "&:after": {
+                position: 'relative',
+                '&:after': {
                   content: "''",
-                  position: "absolute",
+                  position: 'absolute',
                   bottom: 0,
-                  left: "2%",
-                  width: "60%",
-                  height: "3px",
-                  backgroundColor: "goldenrod",
+                  left: '2%',
+                  width: '60%',
+                  height: '3px',
+                  backgroundColor: 'goldenrod',
                 },
               }}
             >
-              {data?.coachName}
+              {coachesData?.firstName}
             </Typography>
           </Typography>
         </Typography>
         <SingleCoachDetail
-          id={data?.id}
-          name={data?.coachName}
-          imgSrc={`${data?.imageUrl}`}
-          descriptionArr={data?.description}
+          id={coachesData?._id}
+          name={coachesData?.firstName}
+          imgSrc={`${coachesData?.image}`}
+          descriptionArr={coachesData?.about}
         />
       </Box>
     </>
