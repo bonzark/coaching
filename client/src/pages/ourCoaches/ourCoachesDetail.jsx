3ce0ed6711604bd9be8729632b5cb7b5a777ee24
bonzark <hello@bonzark.com>
2023-10-03 11:40:24 +0530

    Page banner added in coach detail page.

--- client/src/pages/ourCoaches/ourCoachesDetail.jsx
+++ client/src/pages/ourCoaches/ourCoachesDetail.jsx
@@ -1,8 +1,9 @@
-import { Box, Typography } from '@mui/material';
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import SingleCoachDetail from '../../sections/SingleCoachDetail';
-import { getCoaches } from '../../services/session.service';
+import { Box, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import SingleCoachDetail from "../../sections/SingleCoachDetail";
+import { getCoaches } from "../../services/session.service";
+import PageBanner from "../../sections/PageBanner";
 
 const OurCoachesDetail = () => {
   const { id } = useParams();
@@ -20,32 +21,36 @@ const OurCoachesDetail = () => {
         }
       })
       .catch((err) => {
-        console.log('err', err);
+        console.log("err", err);
       });
   }, [id]);
 
   return (
     <>
+      <PageBanner
+        heading={coachesData?.firstName}
+        imgSrc="../coach-detail.jpeg"
+      />
       <Box
         sx={{
-          margin: '5vh 0',
+          margin: "5vh 0",
           padding: {
-            xs: '4rem 1rem 0',
-            sm: '4rem 2rem 0',
-            md: '4rem 3rem 0',
+            xs: "4rem 1rem 0",
+            sm: "4rem 2rem 0",
+            md: "4rem 3rem 0",
           },
-          background: 'linear-gradient(#DCD9F0,#ffffff)',
+          background: "linear-gradient(#DCD9F0,#ffffff)",
         }}
       >
         <Typography
           variant="h1"
           sx={{
-            textAlign: 'center',
+            textAlign: "center",
             fontSize: {
-              xs: '1.5rem',
-              sm: '2rem',
-              md: '2.5rem',
-              lg: '3rem',
+              xs: "1.5rem",
+              sm: "2rem",
+              md: "2.5rem",
+              lg: "3rem",
             },
           }}
         >
@@ -53,22 +58,22 @@ const OurCoachesDetail = () => {
             variant="span"
             sx={{
               fontFamily: "'montserrat', sans-seri",
-              color: '#671D63',
+              color: "#671D63",
             }}
           >
             Meet Our Coach &nbsp;
             <Typography
               variant="span"
               sx={{
-                position: 'relative',
-                '&:after': {
+                position: "relative",
+                "&:after": {
                   content: "''",
-                  position: 'absolute',
+                  position: "absolute",
                   bottom: 0,
-                  left: '2%',
-                  width: '60%',
-                  height: '3px',
-                  backgroundColor: 'goldenrod',
+                  left: "2%",
+                  width: "60%",
+                  height: "3px",
+                  backgroundColor: "goldenrod",
                 },
               }}
             >
