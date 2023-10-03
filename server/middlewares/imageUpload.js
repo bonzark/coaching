f9243565cced3b5cecdf5720db0e6b78e97025e3
bonzark <hello@bonzark.com>
2023-10-03 11:07:45 +0530

    multer change

--- server/middlewares/imageUpload.js
+++ server/middlewares/imageUpload.js
@@ -1,12 +1,8 @@
 const multer = require("multer");
 
-const storage = multer.diskStorage({
-  destination: "./public/uploads/",
-  filename: (req, file, cb) => {
-    return cb(null, `${file.originalname}`);
-  },
-});
+const storage = multer.memoryStorage();
 const upload = multer({
+  dest: "uploads/",
   storage,
 });
 
