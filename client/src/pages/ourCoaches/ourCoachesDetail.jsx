f77c1db483d888bd88f67eb40e69f23c6f51f592
bonzark <hello@bonzark.com>
2023-10-03 16:16:51 +0530

    Coach detail page UI Fix

--- client/src/pages/ourCoaches/ourCoachesDetail.jsx
+++ client/src/pages/ourCoaches/ourCoachesDetail.jsx
@@ -28,12 +28,12 @@ const OurCoachesDetail = () => {
   return (
     <>
       <PageBanner
-        heading={coachesData?.firstName}
+        heading={`Meet Out Coach ${coachesData?.firstName}`}
         imgSrc="../coach-detail.jpeg"
       />
       <Box
         sx={{
-          margin: "5vh 0",
+          margin: "0 0",
           padding: {
             xs: "4rem 1rem 0",
             sm: "4rem 2rem 0",
@@ -42,45 +42,6 @@ const OurCoachesDetail = () => {
           background: "linear-gradient(#DCD9F0,#ffffff)",
         }}
       >
-        <Typography
-          variant="h1"
-          sx={{
-            textAlign: "center",
-            fontSize: {
-              xs: "1.5rem",
-              sm: "2rem",
-              md: "2.5rem",
-              lg: "3rem",
-            },
-          }}
-        >
-          <Typography
-            variant="span"
-            sx={{
-              fontFamily: "'montserrat', sans-seri",
-              color: "#671D63",
-            }}
-          >
-            Meet Our Coach &nbsp;
-            <Typography
-              variant="span"
-              sx={{
-                position: "relative",
-                "&:after": {
-                  content: "''",
-                  position: "absolute",
-                  bottom: 0,
-                  left: "2%",
-                  width: "60%",
-                  height: "3px",
-                  backgroundColor: "goldenrod",
-                },
-              }}
-            >
-              {coachesData?.firstName}
-            </Typography>
-          </Typography>
-        </Typography>
         <SingleCoachDetail
           id={coachesData?._id}
           name={coachesData?.firstName}
--- client/src/sections/SingleCoachDetail.jsx
+++ client/src/sections/SingleCoachDetail.jsx
@@ -1,40 +1,44 @@
-import { Box, Button, Typography } from '@mui/material';
-import BookSessionBtn from '../components/BookSessionButton';
+import { Box, Button, Typography } from "@mui/material";
+import BookSessionBtn from "../components/BookSessionButton";
 
 const SingleCoachDetail = ({ name, imgSrc, descriptionArr }) => {
   return (
     <Box
       sx={{
-        maxWidth: '1200px',
-        margin: '2rem auto',
-        boxShadow: '0 3px 12px 1px #dfdfdf',
-        borderRadius: '8px',
-        overflow: 'hidden',
-        padding: '1.5rem',
-        backgroundColor: '#fff',
+        maxWidth: "1200px",
+        margin: "2rem auto 3rem",
+        boxShadow: "0 3px 12px 1px #dfdfdf",
+        borderRadius: "8px",
+        overflow: "hidden",
+        padding: "1.5rem",
+        backgroundColor: "#fff",
       }}
     >
       <Box
         sx={{
-          float: { xs: 'none', md: 'right' },
-          margin: '1.2rem',
-          borderRadius: '8px',
-          overflow: 'hidden',
-          display: 'flex',
+          float: { xs: "none", md: "right" },
+          margin: "1.2rem",
+          borderRadius: "8px",
+          overflow: "hidden",
+          display: "flex",
         }}
       >
         <img
           src={imgSrc}
           alt="coach"
           style={{
-            width: '100%',
-            height: 'auto',
+            width: "100%",
+            height: "auto",
           }}
         />
       </Box>
       <Typography paragraph>{name},</Typography>
       {descriptionArr?.map((para, index) => (
-        <Typography paragraph key={index} sx={{ textAlign: { xs: 'left', sm: 'justify' } }}>
+        <Typography
+          paragraph
+          key={index}
+          sx={{ textAlign: { xs: "left", sm: "justify" } }}
+        >
           {para}
         </Typography>
       ))}
@@ -53,7 +57,7 @@ const SingleCoachDetail = ({ name, imgSrc, descriptionArr }) => {
             Gain Awareness Of Your Creation in less than 30 days
           </>
         }
-        bookText={'Book Session'}
+        bookText={"Book Session"}
       />
     </Box>
   );
