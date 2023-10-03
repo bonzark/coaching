7ff40e8c4575ff46b71fa61625d19938033eeb62
bonzark <hello@bonzark.com>
2023-10-03 14:09:02 +0530

    updated modal

--- client/src/components/SessionCard.jsx
+++ client/src/components/SessionCard.jsx
@@ -31,7 +31,7 @@ const SessionCard = ({
         justifyContent: "space-between",
         boxShadow:
           "rgba(9, 30, 66, 0.25) 0px 4px 8px -2px, rgba(9, 30, 66, 0.08) 0px 0px 0px 1px",
-        borderRadius: "8px",
+        borderRadius: "7px",
         transition: "transform 0.35s ease-in-out",
         "&:hover": {
           transform: "scale(1.002)",
