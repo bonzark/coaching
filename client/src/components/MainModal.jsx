dbbf079180cdb386a604fd29c4d752bf00d2d2d6
bonzark <hello@bonzark.com>
2023-10-04 15:12:32 +0530

    css changes

--- client/src/components/MainModal.jsx
+++ client/src/components/MainModal.jsx
@@ -24,7 +24,7 @@ export const MainModal = ({ open, handleClose, blur, children, lg }) => {
           top: "50%",
           left: "50%",
           transform: "translate(-50%, -50%)",
-          width: { xs: lg ? "400px" : "240px", md: lg ? "800px" : "370px" },
+          width: { xs: lg ? "370px" : "240px", md: lg ? "800px" : "400px" },
           bgcolor: "background.paper",
           boxShadow: 24,
           p: { xs: 2, sm: 3, md: 4 },
