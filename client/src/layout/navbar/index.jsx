0f832ad0279686cd91c1f1b8d4b6141179529346
Bonzark <hello@bonzark.com>
2023-10-04 11:40:54 +0530

    navbar issue fixed

--- client/src/layout/navbar/index.jsx
+++ client/src/layout/navbar/index.jsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import * as React from 'react';
 import {
   AppBar,
   Box,
@@ -11,13 +11,14 @@ import {
   ListItemButton,
   Toolbar,
   Typography,
-} from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
-import { Link } from "react-router-dom";
-import FormModal from "../../sections/FormModal";
-import { getAuthToken } from "../../utils/auth";
-import { navItems, sidebarItems } from "../../utils/constant";
-import CommonDropdown from "../../components/DropDown";
+} from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Link } from 'react-router-dom';
+import FormModal from '../../sections/FormModal';
+import { getAuthToken } from '../../utils/auth';
+import { navItems, sidebarItems } from '../../utils/constant';
+import CommonDropdown from '../../components/DropDown';
+import EventEmitter from 'reactjs-eventemitter';
 
 const Navbar = () => {
   const drawerWidth = 300;
@@ -41,6 +42,16 @@ const Navbar = () => {
     }
   }, [getAuthToken()]);
 
+  React.useEffect(() => {
+    EventEmitter.subscribe('loginSuccess', (event) => {
+      setIsLoggedIn(true);
+    });
+
+    EventEmitter.subscribe('logoutSuccess', (event) => {
+      setIsLoggedIn(false);
+    });
+  }, []);
+
   const handleOpen = () => setOpen(true);
 
   const handleClose = () => {
@@ -54,24 +65,24 @@ const Navbar = () => {
       setColorchange(false);
     }
   };
-  window.addEventListener("scroll", changeNavbarColor);
+  window.addEventListener('scroll', changeNavbarColor);
 
   const drawer = (
     <Box>
       <Box
         sx={{
-          background: "linear-gradient(#DCD9F0,#ffffff)",
-          padding: "0.5rem",
+          background: 'linear-gradient(#DCD9F0,#ffffff)',
+          padding: '0.5rem',
         }}
       >
         <Box
           sx={{
-            display: "flex",
-            borderRadius: "100px",
-            overflow: "hidden",
-            maxWidth: "fit-content",
-            margin: "0rem auto",
-            border: "5px solid #fff",
+            display: 'flex',
+            borderRadius: '100px',
+            overflow: 'hidden',
+            maxWidth: 'fit-content',
+            margin: '0rem auto',
+            border: '5px solid #fff',
           }}
         >
           <Link to="/" onClick={handleDrawerToggle}>
@@ -79,10 +90,10 @@ const Navbar = () => {
               src="/becomeYourCreator.jpeg"
               alt="logo"
               style={{
-                display: "flex",
-                maxWidth: "100px",
-                height: "auto",
-                margin: "auto",
+                display: 'flex',
+                maxWidth: '100px',
+                height: 'auto',
+                margin: 'auto',
               }}
             />
           </Link>
@@ -92,19 +103,19 @@ const Navbar = () => {
       <List>
         {navItems.map((item) => (
           <ListItem key={item.name} disablePadding onClick={handleDrawerToggle}>
-            <ListItemButton sx={{ textAlign: "center", color: "#673d67" }}>
+            <ListItemButton sx={{ textAlign: 'center', color: '#673d67' }}>
               <Button
                 component={Link}
                 to={item.link}
                 onClick={() => {
                   window.scroll({
                     top: 0,
-                    behavior: "smooth",
+                    behavior: 'smooth',
                   });
                 }}
                 sx={{
-                  color: "#673d67",
-                  margin: "0 auto",
+                  color: '#673d67',
+                  margin: '0 auto',
                 }}
               >
                 {item.name}
@@ -113,9 +124,9 @@ const Navbar = () => {
           </ListItem>
         ))}
       </List>
-      <Box sx={{ display: "grid", gap: "20px" }}>
+      <Box sx={{ display: 'grid', gap: '20px' }}>
         {isLoggedIn ? (
-          <Box sx={{ display: { xs: "block", md: "none" } }}>
+          <Box sx={{ display: { xs: 'block', md: 'none' } }}>
             <CommonDropdown
               isMobile={mobileOpen}
               dropdownItems={sidebarItems}
@@ -126,16 +137,16 @@ const Navbar = () => {
           <Button
             variant="outlined"
             sx={{
-              color: "#671d63",
-              backgroundColor: "white",
-              minWidth: "130px",
-              margin: "0 auto",
-              fontWeight: "bolder",
-              border: "1px solid #671d63",
-              ":hover": {
-                backgroundColor: "#671d63",
-                color: "white",
-                border: "1px solid transparent",
+              color: '#671d63',
+              backgroundColor: 'white',
+              minWidth: '130px',
+              margin: '0 auto',
+              fontWeight: 'bolder',
+              border: '1px solid #671d63',
+              ':hover': {
+                backgroundColor: '#671d63',
+                color: 'white',
+                border: '1px solid transparent',
               },
             }}
             onClick={handleOpen}
@@ -150,9 +161,9 @@ const Navbar = () => {
   return (
     <Box
       sx={{
-        position: "sticky",
-        width: "100%",
-        height: "100%",
+        position: 'sticky',
+        width: '100%',
+        height: '100%',
         top: 0,
         right: 0,
         left: 0,
@@ -160,42 +171,42 @@ const Navbar = () => {
       }}
     >
       <FormModal open={open} handleClose={handleClose} />
-      <Box sx={{ display: "flex" }}>
+      <Box sx={{ display: 'flex' }}>
         <AppBar
           component="nav"
           sx={{
-            background: !colorChange ? "#873785" : "#671d63",
+            background: !colorChange ? '#873785' : '#671d63',
             boxShadow: colorChange
               ? {
-                  lg: "#32325d3f 0px 50px 100px -20px, #0000004c 0px 30px 60px -30px",
+                  lg: '#32325d3f 0px 50px 100px -20px, #0000004c 0px 30px 60px -30px',
                 }
-              : "none",
-            zIndex: "2",
-            position: "static",
-            padding: "0 !important",
+              : 'none',
+            zIndex: '2',
+            position: 'static',
+            padding: '0 !important',
           }}
         >
           <Box
             sx={{
-              display: { xs: "none", md: "block" },
-              marginRight: { xs: 0, md: "auto" },
-              background: "linear-gradient(#DCD9F0,#ffffff)",
-              padding: "5px",
-              margin: "0.5rem 0 0 2rem",
-              borderRadius: "100px",
-              position: "absolute",
+              display: { xs: 'none', md: 'block' },
+              marginRight: { xs: 0, md: 'auto' },
+              background: 'linear-gradient(#DCD9F0,#ffffff)',
+              padding: '5px',
+              margin: '0.5rem 0 0 2rem',
+              borderRadius: '100px',
+              position: 'absolute',
               top: 0,
               left: 0,
             }}
           >
-            <Box sx={{ borderRadius: "50%", overflow: "hidden" }}>
-              <Link to="/" style={{ display: "flex" }}>
+            <Box sx={{ borderRadius: '50%', overflow: 'hidden' }}>
+              <Link to="/" style={{ display: 'flex' }}>
                 <img
                   src="/becomeYourCreator.jpeg"
                   alt="logo"
                   style={{
-                    maxWidth: "100px",
-                    height: "auto",
+                    maxWidth: '100px',
+                    height: 'auto',
                   }}
                 />
               </Link>
@@ -203,10 +214,10 @@ const Navbar = () => {
           </Box>
           <Toolbar
             sx={{
-              paddingLeft: "0 !important",
-              justifyContent: "flex-end",
-              maxWidth: "80%",
-              marginLeft: "auto",
+              paddingLeft: '0 !important',
+              justifyContent: 'flex-end',
+              maxWidth: '80%',
+              marginLeft: 'auto',
             }}
           >
             <IconButton
@@ -216,16 +227,16 @@ const Navbar = () => {
               onClick={handleDrawerToggle}
               sx={{
                 mr: 2,
-                display: { md: "none", xs: mobileOpen ? "none" : "" },
+                display: { md: 'none', xs: mobileOpen ? 'none' : '' },
               }}
             >
               <MenuIcon />
             </IconButton>
             <Box
               sx={{
-                display: { xs: "none", md: "flex" },
-                gap: { md: "5px", lg: "15px" },
-                marginRight: { md: "15px" },
+                display: { xs: 'none', md: 'flex' },
+                gap: { md: '5px', lg: '15px' },
+                marginRight: { md: '15px' },
               }}
             >
               {navItems.map((item) => (
@@ -236,17 +247,17 @@ const Navbar = () => {
                   onClick={() => {
                     window.scroll({
                       top: 0,
-                      behavior: "smooth",
+                      behavior: 'smooth',
                     });
                   }}
                   sx={{
-                    color: "white",
-                    fontWeight: "600",
-                    textAlign: "center",
+                    color: 'white',
+                    fontWeight: '600',
+                    textAlign: 'center',
                     padding: 0,
-                    transition: "all 0.2s linear",
-                    ":hover": {
-                      color: "#cecece",
+                    transition: 'all 0.2s linear',
+                    ':hover': {
+                      color: '#cecece',
                     },
                   }}
                 >
@@ -255,7 +266,7 @@ const Navbar = () => {
               ))}
             </Box>
             {isLoggedIn ? (
-              <Box sx={{ display: { xs: "none", md: "block" } }}>
+              <Box sx={{ display: { xs: 'none', md: 'block' } }}>
                 <CommonDropdown
                   dropdownItems={sidebarItems}
                   handleDrawerToggle={handleDrawerToggle}
@@ -266,17 +277,17 @@ const Navbar = () => {
               <Button
                 variant="outlined"
                 sx={{
-                  color: "#671d63",
-                  backgroundColor: "white",
-                  width: "130px",
-                  fontWeight: "bolder",
-                  border: "1px solid #671d63",
-                  ":hover": {
-                    backgroundColor: "#671d63",
-                    color: "white",
-                    border: "1px solid #fff",
+                  color: '#671d63',
+                  backgroundColor: 'white',
+                  width: '130px',
+                  fontWeight: 'bolder',
+                  border: '1px solid #671d63',
+                  ':hover': {
+                    backgroundColor: '#671d63',
+                    color: 'white',
+                    border: '1px solid #fff',
                   },
-                  display: { xs: "none", md: "block" },
+                  display: { xs: 'none', md: 'block' },
                 }}
                 onClick={handleOpen}
               >
@@ -294,10 +305,10 @@ const Navbar = () => {
               keepMounted: true,
             }}
             sx={{
-              backgroundColor: "grey",
-              display: { xs: "block", md: "none" },
-              "& .MuiDrawer-paper": {
-                boxSizing: "border-box",
+              backgroundColor: 'grey',
+              display: { xs: 'block', md: 'none' },
+              '& .MuiDrawer-paper': {
+                boxSizing: 'border-box',
                 width: drawerWidth,
               },
             }}
